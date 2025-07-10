06f09c79aea021a001f4a70f6129583b11204be8
SomLaplateforme <somnuck.nguyen-thi@laplateforme.io>
2025-07-10 13:07:44 +0200

    ajout variable

---
+++ Jour01/Job01/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+
+<?php
+    $str = "Laplateforme";
+    $str2 = "Vive";
+    $str3 = "!";
+
+    echo "$str2 $str $str3 ";
+
+    $val = 6+4;
+
+    echo "$val";
+
+     $myBool = true;
+     $myBool = false;
+    
+      echo "$myBool"
+
+    ?>
+</body>
+</html>
