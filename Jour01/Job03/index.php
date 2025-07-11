b7e825fbb7d9a8828a3335b2edee917b8abbafe7
SomLaplateforme <somnuck.nguyen-thi@laplateforme.io>
2025-07-11 09:46:05 +0200

    ajout tableau

---
+++ Jour01/Job03/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tableau PHP</title>
+    <link href="style.css" rel="stylesheet"/>
+</head>
+<header>
+    <table>
+    <tr>
+        <td>
+            type
+        </td>
+        <td>
+            nom
+        </td>
+        <td>
+            valeur
+        </td>
+    </tr>    
+    <tr>
+        <td>
+            boolean
+        </td>
+        <td>
+            mybool
+        </td>
+        <td>
+            true
+        </td>
+    </tr>
+    <tr>
+        <td>
+            entier
+        </td>
+        <td>
+            nombre
+        </td>
+        <td>
+           3 
+        </td>
+    </tr>
+    <tr>
+        <td>
+            chaîne caractères
+        </td>
+        <td>
+            texte
+        </td>
+        <td>
+            je suis moi
+        </td>
+    </tr>
+    <tr>
+        <td>
+            virgule flottante
+        </td>
+        <td>
+            nb virgule
+        </td>
+        <td>
+            1.2345
+        </td>
+    </tr>
+    
+  </table>  
+</header>
+<body>
+    
+</body>
+</html>
